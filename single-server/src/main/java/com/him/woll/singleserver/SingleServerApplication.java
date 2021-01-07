a2b9aa734abf19a03bb27a47c72e438b3a9a22ba
黄川 <1119245380@qq.com>
2021-01-07 10:46:39 +0800

    xiaoniaodepangci
    init project

---
+++ single-server/src/main/java/com/him/woll/singleserver/SingleServerApplication.java
@@ -0,0 +1,13 @@
+package com.him.woll.singleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleServerApplication.class, args);
+    }
+
+}
