241e4eba2a091e6f2d15fa9a0c39385effce73cd
huangc <1119245380@qq.com>
2021-04-01 10:26:51 +0800

    app分支

--- single-server-shiro/src/main/java/com/him/woll/singleservershiro/controller/ChatMsgController.java
+++ single-server-shiro/src/main/java/com/him/woll/singleservershiro/controller/ChatMsgController.java
@@ -13,8 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.BlockingQueue;
 
 /**
  * <p>
